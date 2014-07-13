871c6a324c48cc7a5bacd9c829996b5b4fb56aa5
Alexander Kushnarev <avkushnarev@gmail.com>
2014-07-13 12:33:53 +0400

    Fix grunfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
     browserify: {
       js: {
         // A single entry point for our app
-        src: 'app/js/app.js', 'app/js/controllers.js'
+        src: ['app/js/app.js', 'app/js/controllers.js'],
         // Compile to a single file to add a script tag for in your HTML
         dest: 'dist/js/app.js',
       },
