706596d28265f583f2afb6b9a785f19ce82a8b5a
Alexander Kushnarev <avkushnarev@gmail.com>
2014-07-14 17:21:39 +0400

    Refactor auth. Add sign in and log out buttons.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,56 +4,71 @@
 
 var teamHubControllers = angular.module('teamHubControllers', []);
 
-teamHubControllers.controller("AuthCtrl",  function($scope, $routeParams) {
-    console.log($routeParams);
+
+teamHubControllers.controller("AuthCtrl", function($scope, $location) {
+
+    if ($location.path() == '/oauth_callback') {
+
+        OAuth.callback(function(err, result) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                $scope.user_signed = true;
+                $scope.$emit('SignIn', result.access_token);
+                $location.path('/') ;
+            }
+        });
+    };
+
+    $scope.signIn = function () {
+       OAuth.initialize('eeQJ00VZ4JN-FLF5BtWuBOmDXXQ');
+       OAuth.redirect('github', {'cache': true}, 'http://localhost:8000/app/#oauth_callback');
+    };
+
+    $scope.logOut = function () {
+       $scope.user_signed = false;
+       $scope.gitHubToken = '';
+       $scope.repos = [];
+    //    $scope.$apply();
+    };
+
 });
+
 
 teamHubControllers.controller('RepoCtrl', function ($scope, $location) {
     $scope.repos = [];
-    var gitHubToken = '';
+    $scope.$on('SignIn', showRepos);
 
-     if ($location.path() == '/oauth'){
-         OAuth.callback(function(err, result){
-             if (err) {
-                 console.log(err);
-             }
-             else {
-                 gitHubToken = result.access_token;
-                 $location.path('/') ;
-             }
-         });
-     };
+    function showRepos(event, gitHubToken) {
 
-    if (!gitHubToken){
-        OAuth.initialize('eeQJ00VZ4JN-FLF5BtWuBOmDXXQ');
-        OAuth.redirect('github', {'cache': true}, 'http://localhost:8000/app/#oauth')
+        var github = new Github({
+            token: gitHubToken,
+            auth: "oauth"
+        });
+
+        var user = github.getUser();
+
+        user.repos(function (err, repos) {
+            if (err) {
+                console.log(err);
+                return;
+            } else {
+                // TODO: should be refactored
+                $scope.user_avatar = repos[0].owner.avatar_url;
+                $scope.user_name = repos[0].owner.login;
+
+                repos.map(addPullsInfo);
+            }
+        });
+
+        user.orgs(function (err, orgs) {
+            if (orgs) {
+                orgs.map(addOrgRepos);
+            }
+        });
+
     };
-    
-    var github = new Github({
-        token: gitHubToken,
-        auth: "oauth"
-    });
-
-    var user = github.getUser();
-
-    user.repos(function (err, repos) {
-        if (err) {
-            console.log(err);
-            return;
-        } else {
-            // TODO: should be refactored
-            $scope.user_avatar = repos[0].owner.avatar_url;
-            $scope.user_name = repos[0].owner.login;
-            
-            repos.map(addPullsInfo);
-        }
-    });
-
-    user.orgs(function (err, orgs) {
-        if (orgs) {
-            orgs.map(addOrgRepos);
-        }
-    });
 
     function pushToScope(repo) {
         $scope.repos.push(repo);
